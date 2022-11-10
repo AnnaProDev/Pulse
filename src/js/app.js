85d935041f6336e6960006e8e8213204c7c30ffc
Anna Kondrateva <85047120+AnnaKondrDeveloper@users.noreply.github.com>
2022-11-10 13:21:38 -0500

    Add section carousel and mobile adaptation

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,36 @@
 import * as flsFunctions from "./modules/functions.js";
-
+import * as slick from "./slick.min.js";
 flsFunctions.isWebp();
+
+$(document).ready(function(){
+	$('.carousel__inner').slick({
+		dots: false,
+		speed: 1200,
+		//adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrow-left.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-right.png"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		},
+		]
+	});
+})
